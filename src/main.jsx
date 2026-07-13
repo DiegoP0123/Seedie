e7662ff181dd7a9d347d3845dfbfc81f1ee61a04
DiegoP0123 <paucardiego355@gmail.com>
2026-07-12 22:35:56 -0500

    Fix BrowserRouter basename trailing slash

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Seedie">
+    <BrowserRouter basename="/Seedie/">
       <App />
     </BrowserRouter>
   </StrictMode>
